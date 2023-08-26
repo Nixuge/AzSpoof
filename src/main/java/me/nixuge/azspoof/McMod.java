dbebd0e94f857ec4d280eb6015607ce67a7e6d06
Nixuge <mail@nixuge.me>
2023-08-26 17:41:23 +0200

    Bumper version (1.0.0>1.1.0)

--- src/main/java/me/nixuge/azspoof/McMod.java
+++ src/main/java/me/nixuge/azspoof/McMod.java
@@ -24,7 +24,7 @@ import java.io.File;
 public class McMod {
     public static final String MOD_ID = "azspoof";
     public static final String NAME = "AzSpoof";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     
     @Getter
